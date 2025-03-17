865b61483ea05a91da8e8dd430738d27405a78b0
Konrad <kk0xf0@gmail.com>
2025-03-17 01:13:28 +0100

    update atlas

--- atlas.js
+++ atlas.js
@@ -19,7 +19,10 @@ class TextureAtlas {
         const width = image.width;
         const height = image.height;                
         let canvas = new OffscreenCanvas(width, height);
-        let ctx = canvas.getContext("2d");
+        let ctx = canvas.getContext("2d", {
+            willReadFrequently: true
+        });
+        
         ctx.drawImage(image, 0, 0);
         const columns = width / size;
         const textures = [];
@@ -46,12 +49,7 @@ class TextureAtlas {
             textures.push(texture);
         }
 
-        return new TextureAtlas(textures, size);
-        
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        return new TextureAtlas(textures, size);    
     }
 
     /**
@@ -62,33 +60,6 @@ class TextureAtlas {
     bind(gl, textureUnit, idx) {
         gl.activeTexture(gl.TEXTURE0 + textureUnit);
         gl.bindTexture(gl.TEXTURE_2D, this.#textures[idx]);
-    }
-
-    /**
-     * 
-     * @param {Float32Array} output 
-     * @param {number} texColumn 
-     */
-    uvRect(output, texColumn) {
-        output[0] = 0;
-        output[1] = 0;
-        output[2] = 1;
-        output[3] = 0;
-        output[4] = 1;
-        output[5] = 1;
-        output[6] = 0;
-        output[7] = 1;
-        // output[0] = (this.#size * texColumn) / this.#width + this.#delta;
-        // output[1] = this.#delta;
-
-        // output[6] = (this.#size * texColumn) / this.#width + this.#delta;
-        // output[7] = 1 - this.#delta;
-        
-        // output[2] = (this.#size * (texColumn + 1)) / this.#width - this.#delta;
-        // output[5] = 1 - this.#delta;
-        
-        // output[4] = (this.#size * (texColumn + 1)) / this.#width - this.#delta;
-        // output[3] = this.#delta;
     }
 }
 
