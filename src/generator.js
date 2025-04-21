2ac685bf5080c6268855634bd030e4018232bdef
Konrad <kk0xf0@gmail.com>
2025-04-21 12:50:45 +0200

    add ppv

--- src/generator.js
+++ src/generator.js
@@ -6,6 +6,7 @@ import { ImagePixels } from "./utils.js";
 export class PixelDataChunkGenerator {
     #pixels
     #originPosition
+    #ppv = 4
 
     /**
      * @param {ImagePixels} pixels 
@@ -20,16 +21,16 @@ export class PixelDataChunkGenerator {
      * @param {Vec2} chunkPos
      */
     generateChunk(chunkPos) {
-        const startX = this.#originPosition.x + (chunkPos.x * CHUNK_SIZE);
-        const startY = this.#originPosition.y + ((-chunkPos.y - 1) * CHUNK_SIZE);
+        const startX = this.#originPosition.x + (chunkPos.x * CHUNK_SIZE) * this.#ppv;
+        const startY = this.#originPosition.y + ((-chunkPos.y - 1) * CHUNK_SIZE) * this.#ppv;
         const w = this.#pixels.width;
         const h = this.#pixels.height;
         const chunk = new ChunkData();
 
         for (let y = 0; y < CHUNK_SIZE; y++)
             for (let x = 0; x < CHUNK_SIZE; x++) {
-                const cx = startX + x;
-                const cy = startY + y;
+                const cx = startX + x * this.#ppv;
+                const cy = startY + y * this.#ppv;
                 if (cx < 0 || cy < 0 || cx >= w || cy >= h)
                     continue;
                 let height = this.#pixels.getR(cx, cy);
@@ -40,8 +41,8 @@ export class PixelDataChunkGenerator {
                     chunk.set(height - 1, x, CHUNK_SIZE - y - 1, BLOCKS.BLOCK_DIRT_GRASS);
                 if (Math.random() < 0.1)
                     chunk.set(height, x, CHUNK_SIZE - y - 1, BLOCKS.BLOCK_ROCK);
-                if (Math.random() < 0.1)
-                    chunk.set(height + 1, x, CHUNK_SIZE - y - 1, BLOCKS.BLOCK_ROCK);
+                // if (Math.random() < 0.1)
+                //     chunk.set(height + 1, x, CHUNK_SIZE - y - 1, BLOCKS.BLOCK_ROCK);
             }
         return chunk;
     }
