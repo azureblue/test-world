79de225025b8d40dfa02268672655f805c35d19f
KK <kk0xf0@gmail.com>
2026-01-06 01:03:52 +0100

    improving main and camera

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { Camera, FrustumCuller } from "./camera.js";
 import { UIntMesh } from "./chunk.js";
 import { Projection, FVec3, mat4, fvec3 } from "./geom.js";
 import { Program } from "./gl.js";
+import { KeyboardInput, MouseInput } from "./input.js";
 import { FPSCounter } from "./perf.js";
 import { TextureArray } from "./textures.js";
 import { Replacer, Resources, writeVoxelWireframe } from "./utils.js";
 import { BlockLocation, World } from "./world.js";
 
-const VIEW_DISTANCE_SQ = (11 * 15) ** 2;
+const VIEW_DISTANCE_SQ = (4 * 32) ** 2;
 // const VIEW_DISTANCE_SQ = 409600.0;
 
 export async function start() {
@@ -31,8 +32,7 @@ export async function start() {
         gl,
         Replacer.replace(await Resources.loadText("shaders/chunk0.vert"),
             {
-                "viewDistanceSq": VIEW_DISTANCE_SQ.toFixed(1),
-                "edgeShadowDistanceSq": (800.0).toFixed(1)
+                "viewDistanceSq": VIEW_DISTANCE_SQ.toFixed(1)
             }
         ),
         await Resources.loadText("shaders/chunk0.frag")
@@ -64,14 +64,14 @@ export async function start() {
     const bhFBuffer = new Float32Array(72);
     gl.bufferData(gl.ARRAY_BUFFER, bhFBuffer, gl.DYNAMIC_DRAW);
     gl.vertexAttribPointer(aInBH, 3, gl.FLOAT, false, 0, 0);
-    
+
     gl.useProgram(chunk0Program.program);
 
     UIntMesh.setGL(gl, aIn);
     const texArray = TextureArray.create(gl, textures, 16);
-    
+
     const world = new World();
-    
+
     const uCamera = gl.getUniformBlockIndex(chunk0Program.program, "Camera");
     const uCameraSize = gl.getActiveUniformBlockParameter(chunk0Program.program, uCamera, gl.UNIFORM_BLOCK_DATA_SIZE);
     const uCameraBuffer = gl.createBuffer();
@@ -119,83 +119,44 @@ export async function start() {
     world.update();
 
     const currentChunk = await world.getCurrentChunk();
-    const peek = currentChunk.peek(0, 0);
-    const camera = new Camera(new FVec3(0, peek + 2, 0));
+    // const peek = currentChunk.peek(0, 0);
+    const camera = new Camera(new FVec3(0, 120, 0));
     const frustumCuller = new FrustumCuller(projection.frustum, camera);
 
     const cameraSpeed = 0.5;
 
-    const keys = {
-        up: false,
-        down: false,
-        left: false,
-        right: false,
-        shift: false,
-        ctrl: false
-    }
-
     let pause = false;
-
-    document.body.addEventListener("mousemove", (me) => {
-        if (pause)
-            return;
-        camera.changeYaw(me.movementX * 0.2);
-        camera.changePitch(-me.movementY * 0.2);
-    })
-
-    document.body.addEventListener("keydown", (ev) => {
-        if (ev.key == " ") {
-            if (!pause) {
-                pause = true;
-            } else {
-                pause = false;
-            }
+    
+    const mouseInput = new MouseInput(canvas, true);
+    const keyboardInput = new KeyboardInput(document.body, 
+        [" ", "w", "a", "s", "d", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Control"], {
+            " ": (pressed) => pressed && (pause = !pause)
+            
         }
-        if (ev.key == "Control")
-            keys.ctrl = true;
-        else if (ev.key == "w" || ev.key == "ArrowUp")
-            keys.up = true;
-        else if (ev.key == "a" || ev.key == "ArrowLeft")
-            keys.left = true;
-        else if (ev.key == "s" || ev.key == "ArrowDown")
-            keys.down = true;
-        else if (ev.key == "d" || ev.key == "ArrowRight")
-            keys.right = true;
-    }, true);
-
-    document.body.addEventListener("keyup", (ev) => {
-
-        if (ev.key == "Control")
-            keys.ctrl = false;
-        else if (ev.key == "w" || ev.key == "ArrowUp")
-            keys.up = false;
-        else if (ev.key == "a" || ev.key == "ArrowLeft")
-            keys.left = false;
-        else if (ev.key == "s" || ev.key == "ArrowDown")
-            keys.down = false;
-        else if (ev.key == "d" || ev.key == "ArrowRight")
-            keys.right = false;
-    }, true);
-
-    canvas.addEventListener("click", async () => canvas.requestPointerLock());
+    );
+        
 
     const fpsCounter = new FPSCounter();
     const blockLocation = new BlockLocation();
-    const blockLocation2 = new BlockLocation();
     function draw() {
         if (pause) {
             requestAnimationFrame(draw);
             return;
         }
-        const cameraSpeedMultiplier = keys.ctrl ? 0.2 : 1;
-        if (keys.up)
+        const cameraSpeedMultiplier = 1;
+        if (keyboardInput.isKeyPressed("w"))
             camera.moveForward(cameraSpeed * cameraSpeedMultiplier);
-        if (keys.down)
+        if (keyboardInput.isKeyPressed("s"))
             camera.moveForward(-cameraSpeed * cameraSpeedMultiplier);
-        if (keys.left)
+        if (keyboardInput.isKeyPressed("a"))
             camera.moveRight(-cameraSpeed * cameraSpeedMultiplier);
-        if (keys.right)
+        if (keyboardInput.isKeyPressed("d"))
             camera.moveRight(cameraSpeed * cameraSpeedMultiplier);
+
+        const mouseDelta = mouseInput.getDeltaAndReset()
+        camera.changeYaw(mouseDelta.x * 0.2);
+        camera.changePitch(-mouseDelta.y * 0.2);
+        camera.update();
 
         camera.setLookAtMatrix(mView);
 
@@ -212,7 +173,7 @@ export async function start() {
         let chunksDrawn = 0;
         texArray.bind(gl);
         world.update();
-     
+
         world.render(chunk => {
             allChunks++;
             if (!frustumCuller.shouldDraw(chunk)) {
@@ -232,29 +193,34 @@ export async function start() {
         const dir = camera.direction;
 
         const out = world.raycasti(pos, dir, 5);
-            if (out !== null) {
-                // gl.disable(gl.DEPTH_TEST);
-                world.blockAtWorldIPos(fvec3(out.x, out.y, out.z));
-                // console.log(out.value);
-                gl.useProgram(blockHighlightProgram.program);
-                gl.bindBuffer(gl.ARRAY_BUFFER, bhBuffer);
-                writeVoxelWireframe(bhFBuffer, out.x, out.y, out.z);
-                gl.bufferData(gl.ARRAY_BUFFER, bhFBuffer, gl.DYNAMIC_DRAW);
-                gl.vertexAttribPointer(aInBH, 3, gl.FLOAT, false, 0, 0);
-                gl.drawArrays(gl.LINES, 0, 24);
-                // gl.enable(gl.DEPTH_TEST);
-            }
+        if (out !== null) {
+            // gl.disable(gl.DEPTH_TEST);
+            world.blockAtWorldIPos(fvec3(out.x, out.y, out.z));
+            // console.log(out.value);
+            gl.useProgram(blockHighlightProgram.program);
+            gl.bindBuffer(gl.ARRAY_BUFFER, bhBuffer);
+            writeVoxelWireframe(bhFBuffer, out.x, out.y, out.z);
+            gl.bufferData(gl.ARRAY_BUFFER, bhFBuffer, gl.DYNAMIC_DRAW);
+            gl.vertexAttribPointer(aInBH, 3, gl.FLOAT, false, 0, 0);
+            gl.drawArrays(gl.LINES, 0, 24);
+            // gl.enable(gl.DEPTH_TEST);
+        }
 
-        
+
         if (fpsCounter.getCurrentFrame() % 5 === 0) {
             world.blockLocation(blockLocation, camera.position);
             const block = world.blockAtPos(camera.position);
-            
-            statsDiv.textContent = `position x:${pos.x.toFixed(1)} z:${pos.z.toFixed(1)} y:${pos.y.toFixed(1)} ` +
-                `direction x:${dir.x.toFixed(1)} z:${dir.z.toFixed(1)} y:${dir.y.toFixed(1)} ` +
-                ` pitch:${camera.pitch.toFixed(1)} yaw:${camera.yaw.toFixed(1)} fps: ${fpsCounter.fps()} chunks: ${chunksDrawn}/${allChunks}` +
-                `chunk x:${blockLocation.chunkPos.x} y:${blockLocation.chunkPos.y} block pos x:${blockLocation.blockInChunkPos.x} y:${blockLocation.blockInChunkPos.y} z:${blockLocation.blockInChunkPos.z} block: ${block} ` +
-                `${out === null ? "" : "looking at: " + out.block + " at: " + out.x + " " + out.y + " " + out.z}`;
+
+            statsDiv.textContent = 
+                `position ${pos.x.toFixed(1)}x ${pos.z.toFixed(1)}z ${pos.y.toFixed(1)}y ` +
+                `direction ${dir.x.toFixed(1)}x ${dir.z.toFixed(1)}z ${dir.y.toFixed(1)}y ` +
+                `camera ${camera.pitch.toFixed(1)}p ${camera.yaw.toFixed(1)}y ` +
+                `fps ${fpsCounter.fps()} ` +
+                `chunks ${chunksDrawn}/${allChunks}/${world.chunksInView} ` +
+                `chunk x:${blockLocation.chunkPos.x} y:${blockLocation.chunkPos.y} z:${blockLocation.chunkPos.z} ` +
+                `pos x:${blockLocation.blockInChunkPos.x} y:${blockLocation.blockInChunkPos.y} z:${blockLocation.blockInChunkPos.z} ` +
+                `block ${block} ` +
+                `${out === null ? "" : "looking at " + out.block + " at " + out.x + " " + out.y + " " + out.z}`;
         }
         requestAnimationFrame(draw);
         fpsCounter.frame();
