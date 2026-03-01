6e58e8921b48c514c870239db17423be69ad3a33
Konrad <kk0xf0@gmail.com>
2026-03-01 20:33:37 +0100

    update int.h

--- src/mesher/cpp/int.h
+++ src/mesher/cpp/int.h
@@ -1,5 +1,8 @@
 #pragma once
 typedef unsigned int uint;
-// typedef int i32;
+typedef unsigned long long uint64;
+typedef long long int64;
 static_assert(sizeof(uint) == 4, "uint must be 32-bit");
 static_assert(sizeof(int) == 4, "i32 must be 32-bit");
+static_assert(sizeof(uint64) == 8, "uint64 must be 64-bit");
+static_assert(sizeof(int64) == 8, "int64 must be 64-bit");
