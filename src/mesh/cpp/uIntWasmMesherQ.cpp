4f60d3395eb36adc25919a9b6e38cc4642b7e558
Konrad <kk0xf0@gmail.com>
2026-05-02 20:03:03 +0200

    optimizing

--- src/mesh/cpp/uIntWasmMesherQ.cpp
+++ src/mesh/cpp/uIntWasmMesherQ.cpp
@@ -10,37 +10,33 @@ extern "C"
     for (uint h = 1; h < CHUNK_SIZE + 1; h++) {
         for (uint y = 1; y < CHUNK_SIZE + 1; y++) {
             for (uint x = 1; x < CHUNK_SIZE + 1; x++) {
-                uint64 pos_bits = quad_encoder::encode_pos_bits(x - 1, y - 1, h - 1);
                 uint block_data = data.get_hxy(h, x, y);
-                uint block_id = decode_block_id(block_data);
-                if (block_id == BLOCK_EMPTY) {
+                if (block_data == BLOCK_EMPTY) {
                     continue;
                 }
+                uint block_id = decode_block_id(block_data);
 
                 const uint(&block_textures)[6] = BLOCKS_TEXTURES[block_id];
-                if (is_x_quads(block_data)) {                    
+                if (is_x_quads(block_data)) {
                     x_quads_encoder::encode_quad<Direction::Diagonal0>(buffers.mesh_cutout_x_cur, x - 1, y - 1, h - 1, block_textures[1]);
                     x_quads_encoder::encode_quad<Direction::Diagonal1>(buffers.mesh_cutout_x_cur, x - 1, y - 1, h - 1, block_textures[2]);
                     continue;
                 }
 
-                uint is_water = block_id == BLOCK_WATER;
-                auto& face_buffer = is_water ? buffers.mesh_water_cur : buffers.mesh_solid_cur;
                 uint above = data.get_hxy(h + 1, x, y);
-                if (!is_solid(above)) {
-                    if (is_water) {
-                        if (decode_block_id(above) == BLOCK_WATER) 
-                            continue;
-                        water_encoder::encode_face_q<Direction::Up>(face_buffer, x - 1, y - 1, h - 1, block_textures[Direction::Up]);
-                        continue;
+                if (block_id == BLOCK_WATER) {
+                    if (!is_solid(above) && decode_block_id(above) != BLOCK_WATER) {
+                        water_encoder::encode_face_q<Direction::Up>(
+                            buffers.mesh_water_cur, x - 1, y - 1, h - 1, block_textures[Direction::Up]);
                     }
-                    
+                    continue;
+                }
+                auto& face_buffer = buffers.mesh_solid_cur;
+                uint64 pos_bits = quad_encoder::encode_pos_bits(x - 1, y - 1, h - 1);
+
+                if (!is_solid(above)) {
                     uint shadows = aos::compute<Direction::Up>(data, h, x, y);
                     quad_encoder::encode_face_q<Direction::Up>(face_buffer, pos_bits, block_textures[Direction::Up], shadows);
-                }
-
-                if (is_water) {
-                    continue;
                 }
 
                 if (!is_solid(data.get_hxy(h - 1, x, y))) {
